9047eb7132e73e8d00c6cddf1f8feb8a41a88531
Ines Jelovac <ines.jelovac@smartivo.com>
2018-03-03 15:09:03 +0100

    Burger component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,14 @@
 import React, { Component, Fragment } from 'react';
 
+import Burger from '../../components/Burger/Burger';
+
 class BurgerBuilder extends Component {
 
     render() {
         return (
             <Fragment>
                 <div>
-                    Burger
+                    <Burger></Burger>
                 </div>
                 <div>
                     Build Controls
