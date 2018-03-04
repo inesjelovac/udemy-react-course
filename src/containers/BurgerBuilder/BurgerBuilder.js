87d41c6e4f6f3a232abd0686b1c66b8b630bdc7d
Ines Jelovac <ines.jelovac@smartivo.com>
2018-03-04 21:15:28 +0100

    build controls in BurgerBuilder component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
 
@@ -20,7 +21,7 @@ class BurgerBuilder extends Component {
                     <Burger ingredients={this.state.ingredients}></Burger>
                 </div>
                 <div>
-                    Build Controls
+                    <BuildControls />
                 </div>
             </Fragment>
         );
