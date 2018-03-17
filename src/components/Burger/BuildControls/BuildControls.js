f18e5ab51516fbe8b6897dadec65a3b1f7752c32
Ines Jelovac <ines.jelovac@smartivo.com>
2018-03-17 14:15:46 +0100

    adding and removing ingredients

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -15,7 +15,13 @@ const buildControls = (props) => {
     return (
         <div className={classes.BuildControls}>
             {controls.map(ctrl => (
-                <BuildControl key={ctrl.label} label={ctrl.label}></BuildControl>
+                <BuildControl
+                    key={ctrl.label}
+                    label={ctrl.label}
+                    added={() => props.ingredientAdded(ctrl.type)}
+                    removed={() => props.ingredientRemove(ctrl.type)}
+                    disabled={props.disabled[ctrl.type]}
+                ></BuildControl>
             ))}
         </div>
     );
